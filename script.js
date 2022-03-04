22c585906523d09e4e19a39968df87d16081aa7c
itsagift <anna.c.reynolds@gmail.com>
2022-03-04 01:17:50 -0500

    added images

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-var one = document.getElementById('dot-one');
-var two = document.getElementById('dot-two');
-var three = document.getElementById('dot-three');
+const one = document.getElementById('dot-1');
+const two = document.getElementById('dot-2');
+const three = document.getElementById('dot-3');
+
+const dots = document.getElementsByClassName("dot");
+const slides = document.getElementsByClassName("slide");
+
+one.addEventListener("click", function(){ 
+    currentSlide(0);
+});
+two.addEventListener("click", function(){ 
+    currentSlide(1);
+ });
+ three.addEventListener("click", function(){
+    currentSlide(2);
+ })
+
+ function currentSlide(n){
+    
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active--dot");
+    }
+    dots[n].className += " active--dot";
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+    }
+    slides[n].className += " active";
+    
+    console.log(slides);
+ }
+
+ 
